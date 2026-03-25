7bad3bdcab2d895d8e22329c3cbfc6d25fe53279
Lucy <lucinda.paddock@gmail.com>
2026-03-24 22:57:23 -0400

    plotting function

---
+++ src/plot.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+
+def plot_bin_counts(
+    x,
+    y,
+    bin_range=(0, 1000)
+):
+    """
+    Args:
+        x (array): downsampled or predicted counts per bin
+        y (array): true counts per bin
+        bin_range (tuple): range of bin indices to plot
+    """
+    
+    fig, ax = plt.subplots()
+    sub_x = x[bin_range[0]:bin_range[1]]
+    sub_y = y[bin_range[0]:bin_range[1]]
+    bins = [i for i in range(bin_range[0],bin_range[1])]
+    ax.bar(x = bins,
+            height = (sub_y),
+            color = 'blue',
+            alpha = 0.8)
+    ax.bar(x = bins,
+            height = (sub_x),
+            color = 'red',
+            alpha = 0.8)
+    ax.set(xlabel = 'Chr19 Bin Index',
+           ylabel = 'Counts in Bin')
+
+    return fig
